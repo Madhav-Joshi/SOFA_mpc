4960823d19bce93c4399ca736830bbdfd3bb6f27
Madhav-Joshi <madhav16701@gmail.com>
2023-01-03 09:35:12 +0530

    Added files to edit for making MPC controller

---
+++ Wakrabot/control_wakrabot_cables.py
@@ -0,0 +1,166 @@
+import Sofa
+from control_wakrabot_cables_controller import CableController
+
+# Choose in your script to activate or not the GUI
+USE_GUI = True
+vol_mesh_path = "D:\Sofa\Wakrabot\mesh\Wakrabot_final_SI.msh"
+# Control varible to be used for cable
+Control_variable = 'force' # 'displacement' also works
+
+def createScene(root):
+    root.addObject('RequiredPlugin', pluginName=[
+                            "Sofa.Component.LinearSolver.Iterative",  # Needed to use components [CGLinearSolver]
+                            "Sofa.Component.Mapping.Linear",  # Needed to use components [IdentityMapping]
+                            "Sofa.Component.StateContainer",  # Needed to use components [MechanicalObject]
+                            "Sofa.Component.Topology.Container.Dynamic",  # Needed to use components [TetrahedronSetGeometryAlgorithms, TetrahedronSetTopologyContainer]
+                            "Sofa.Component.IO.Mesh", # Needed to use components [MeshGmshLoader] 
+                            "Sofa.Component.Mass", # Needed to use components [MeshMatrixMass] 
+                            "Sofa.Component.MechanicalLoad", # Needed to use components [ConstantForceField] 
+                            "Sofa.Component.ODESolver.Backward", # Needed to use components [EulerImplicitSolver] 
+                            "Sofa.Component.SolidMechanics.FEM.Elastic", # Needed to use components [TetrahedronFEMForceField] 
+                            "Sofa.GL.Component.Rendering3D", # Needed to use components [OglModel] 
+                            "Sofa.Component.Engine.Select", # Needed to use components [BoxROI]
+                            "Sofa.Component.SolidMechanics.Spring", # Needed to use components [RestShapeSpringsForceField]
+                            "Sofa.Component.LinearSolver.Direct", # Needed to use components [SparseLDLSolver]
+                            "Sofa.Component.Constraint.Lagrangian.Correction", # Needed to use components [GenericConstraintCorrection]
+                            "Sofa.Component.AnimationLoop", # Needed to use components [FreeMotionAnimationLoop]
+                            "Sofa.Component.AnimationLoop", # Needed to use components [FreeMotionAnimationLoop]
+                            "Sofa.Component.Visual", # Needed to use components [VisualStyle]
+                            "Sofa.Component.Constraint.Lagrangian.Solver", # Needed to use components [GenericConstraintSolver]
+                            
+                        ])  
+    
+    root.addObject("RequiredPlugin", name='SoftRobots')
+    root.addObject("RequiredPlugin", name='SofaPython3')
+    root.gravity=[0,-9.810,0]
+    root.dt=0.05 # sets the units for time
+    
+    ## root.addObject("DefaultAnimationLoop")
+    root.addObject("FreeMotionAnimationLoop")
+    root.addObject("DefaultVisualManagerLoop")
+    root.addObject('GenericConstraintSolver', tolerance=1e-5, maxIterations=100)
+    
+    root.addObject('VisualStyle', displayFlags="showInteractionForceFields showVisual")
+    root.addObject("MeshGmshLoader",name="meshLoaderCoarse",filename=vol_mesh_path)
+    
+    # New Node
+    wakra = root.addChild("Wakrabot") # Name of child wakra
+    wakra.addObject("EulerImplicitSolver", name='odesolver', rayleighMass=0.1, rayleighStiffness=0.1) # Solver
+    # wakra.addObject("CGLinearSolver",iterations="200",tolerance="1e-09",threshold="1e-9")
+    wakra.addObject('SparseLDLSolver', template='CompressedRowSparseMatrixMat3x3d')
+
+    wakra.addObject("TetrahedronSetTopologyContainer",name="topo",src="@../meshLoaderCoarse")
+    wakra.addObject("TetrahedronSetGeometryAlgorithms",template="Vec3d",name="GeomAlgo")
+
+    wakra.addObject("MechanicalObject",template="Vec3d",name="MechanicalModel",showObject="1",translation="0 0 0")
+    wakra.addObject("TetrahedronFEMForceField",name="FEM",youngModulus="1000000000",poissonRatio="0.1")
+
+    # wakra.addObject("MeshMatrixMass",massDensity="1000")
+    wakra.addObject("UniformMass",totalMass="1") # Approximation
+
+    wakra.addObject('BoxROI', name='roi', box=[-0.050, -0.0023, -0.050, 0.050, 0.0001, 0.050], drawBoxes=True)
+    # RestShapeSpringsForceField is one way in Sofa to implement fixed point constraint.
+    # Here the constraints are applied to the DoFs selected by the previously defined BoxROI
+    wakra.addObject('RestShapeSpringsForceField', points=wakra.roi.indices.getLinkPath(), stiffness=1e12)
+    
+    wakra.addObject('GenericConstraintCorrection')
+
+    ##########################################
+    #######       Cable       ################
+    ##########################################
+    ############  Cable0    ##################
+    
+    cable0 = wakra.addChild('cable0')
+
+    number_of_cable_through_points = 14
+    valuetype = Control_variable
+    cable0.addObject('MechanicalObject', name = 'cable0MO',
+                    position=[[0,-0.022*j-0.002,0.04599] for j in range(number_of_cable_through_points)],
+                    rotation='0 0 0')
+
+    cable0.addObject('CableConstraint', name="CableActuator0",
+                    indices=list(range(0, number_of_cable_through_points)),
+                    minForce=0,  # Set that the cable0 can't push
+                    pullPoint=[0, 0.022  , 0.04599],
+                    value=0,#initialValue
+                    valueType=valuetype, # 'displacement' or 'force'
+                    hasPullPoint=True)
+
+    cable0.addObject('BarycentricMapping',name="mech_baryCentricMap")
+    
+    ############  Cable1    ##################
+    cable1 = wakra.addChild('cable1')
+
+    number_of_cable_through_points = 14
+    cable1.addObject('MechanicalObject', name = 'cable1MO',
+                    position=[[0,-0.022*j-0.002,0.04599] for j in range(number_of_cable_through_points)],
+                    rotation='0 120 0')
+
+    cable1.addObject('CableConstraint', name="CableActuator1",
+                    indices=list(range(0, number_of_cable_through_points)),
+                    minForce=0,  # Set that the cable0 can't push
+                    pullPoint=[0.03982, 0.022  , -0.02299],
+                    value=0, #initialValue
+                    valueType=valuetype, # or 'force'
+                    hasPullPoint=True)
+
+    cable1.addObject('BarycentricMapping',name="mech_baryCentricMap")
+    
+    ############  Cable2    ##################
+    cable2 = wakra.addChild('cable2')
+
+    number_of_cable_through_points = 14
+    cable2.addObject('MechanicalObject', name = 'cable2MO',
+                    position=[[0,-0.022*j-0.002,0.04599] for j in range(number_of_cable_through_points)],
+                    rotation='0 240 0')
+
+    cable2.addObject('CableConstraint', name="CableActuator2",
+                    indices=list(range(0, number_of_cable_through_points)),
+                    minForce=0,  # Set that the cable0 can't push
+                    pullPoint=[-0.03982, 0.022  , -0.02299],
+                    value=0,#initialValue
+                    valueType=valuetype, # or 'force'
+                    hasPullPoint=True)
+
+    cable2.addObject('BarycentricMapping',name="mech_baryCentricMap")
+    
+    wakra.addObject(CableController(name="CableController", wakrabot=wakra, rootnode = root))
+    
+    ############################################
+    ######  Visualisation Node #################
+    ############################################
+
+    # A subnode visu_wakra of wakra
+    visu_wakra = wakra.addChild("Visual_wakra")
+    visu_wakra.addObject("OglModel",name="VisualModel_wakra",src="@../../meshLoaderCoarse",color="green")
+    visu_wakra.addObject("IdentityMapping",name="Mapping_wakra",input="@../MechanicalModel",output="@VisualModel_wakra")
+    
+    return root
+
+def main():
+    import Sofa.Gui
+    root = Sofa.Core.Node("root") # Create the root node
+    # Call the below 'createScene' function to create the scene graph
+    createScene(root)
+    # Once defined, initialization of the scene graph
+    Sofa.Simulation.init(root)
+    # Run the simulation for 10 time steps
+    if not USE_GUI:
+        for iteration in range(10):
+            print(f'Iteration #{iteration}')
+            Sofa.Simulation.animate(root, root.dt.value)
+    else:
+        # Find out the supported GUIs
+        print ("Supported GUIs are: " + Sofa.Gui.GUIManager.ListSupportedGUI(","))
+        # Launch the GUI (qt or qglviewer)
+        Sofa.Gui.GUIManager.Init("myscene", "qglviewer")
+        Sofa.Gui.GUIManager.createGUI(root, __file__)
+        # Sofa.Gui.GUIManager.SetDimension(1080, 1080)
+        # Initialization of the scene will be done here
+        Sofa.Gui.GUIManager.MainLoop(root)
+        Sofa.Gui.GUIManager.closeGUI()
+        print("GUI was closed")
+    print("Simulation is done.")
+
+if __name__=="__main__":
+    main()
---
+++ Wakrabot/control_wakrabot_cables_controller.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import Sofa.Core
+import numpy as np
+
+class CableController(Sofa.Core.Controller):
+    
+    def __init__(self, *args, **kwargs):
+        Sofa.Core.Controller.__init__(self, *args, **kwargs)
+        print('Controller Initialized')
+        self.root = kwargs.get("rootnode")
+        self.wakra = kwargs.get('wakrabot')
+        return
+
+    def onKeypressedEvent(self,e):
+        inputValue = [  self.wakra.cable0.CableActuator0.value,
+                        self.wakra.cable1.CableActuator1.value,
+                        self.wakra.cable2.CableActuator2.value]
+        
+        if self.wakra.cable0.CableActuator0.valueType.value == 'force': scale = 2e-2
+        else: scale = 1e-3 # 1e-3 for displacement control
+
+        ctrl0 = inputValue[0].value
+        if e["key"] == '=':
+            ctrl0 = inputValue[0].value[0] + scale
+            
+        elif e["key"] == '-':
+            ctrl0 = inputValue[0].value[0] - scale
+        inputValue[0].value = [ctrl0]
+
+        ctrl1 = inputValue[1].value
+        if e["key"] == ']':
+            ctrl1 = inputValue[1].value[0] + scale
+            
+        elif e["key"] == '[':
+            ctrl1 = inputValue[1].value[0] - scale
+        inputValue[1].value = [ctrl1]
+
+        ctrl2 = inputValue[2].value
+        if e["key"] == "'":
+            ctrl2 = inputValue[2].value[0] + scale
+            
+        elif e["key"] == ";":
+            ctrl2 = inputValue[2].value[0] - scale
+        inputValue[2].value = [ctrl2]
+        
+        pos = self.wakra.cable0.cable0MO.position.value
+        print('Length =',np.sum(np.linalg.norm(pos[1:]-pos[:-1],axis=1)))
+        # print("Cable length value is", [ctrl0,ctrl1,ctrl2])
+
+    def onAnimateBeginEvent(self, event):
+        #do whatever you want at the beginning of the step
+        t = self.root.time.value
+        # print(f'Begin Animate Event Called at {np.round(t,2)}')
+
+    # called on each animation step
+    def onAnimateEndEvent(self, event): 
+        #access the 'position' state vector
+        myMOpositions = self.wakra.MechanicalModel.position.value[[1912,1916,1914,1915,1911,1913]]
+        # print the first value of the DOF 0 (Vec3 : x,y,z) x[0] y[0] z[0]
+        # print('Coordinates of front',str((myMOpositions[0]+myMOpositions[1])/2))
+        # print('Coordinates of right',str((myMOpositions[2]+myMOpositions[3])/2))
+        # print('Coordinates of leftt',str((myMOpositions[4]+myMOpositions[5])/2))
+        pts = np.array([    (myMOpositions[0]+myMOpositions[1])/2,
+                            (myMOpositions[2]+myMOpositions[3])/2,
+                            (myMOpositions[4]+myMOpositions[5])/2,
+                            ])
+        center = np.mean(pts,axis=0)
+        direction = np.cross(pts[2]-pts[0],pts[1]-pts[0])
+        direction = direction/np.linalg.norm(direction)
+        
+        h = 0.400 # height of floor from the base of the wakrabot
+        laser_coord = center + (h-(-center[1]))/(-direction[1])*direction
+        # print('Laser Coordinates',laser_coord)
+        return
